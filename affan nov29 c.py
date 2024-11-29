25766257ce12d0e435c6c66fc30d415ea3395f02
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2024-11-29 16:05:02 +0530

    Add files via upload

---
+++ affan nov29 c.py
@@ -0,0 +1,7 @@
+num=int(input("Enter a number"))
+def recursive_factorial(num):
+    if num==1:
+        return 1
+    else:
+        return num*recursive_factorial(num-1)
+print(recursive_factorial(num))
