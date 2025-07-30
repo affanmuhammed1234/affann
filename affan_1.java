4e3d453cd065222051a17215c4d8cc2d00a65205
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2025-07-30 13:53:32 +0530

    Add files via upload

--- affan_1.java
+++ affan_1.java
@@ -1,10 +1,41 @@
-package affanP1 ;
+package affan;
 
-public class affan_1 {
-	public static void main(String [] args) {
-		System.out.println("hellaa \n java");
-	}
+class Student{
+	int rollnumber;
+	String name;
+	int m1,m2,m3;
+	float avg;
 	
+	Student( String name,int rollnumber , int m1 , int m2 , int m3){
+		this.name = name;
+		this.rollnumber = rollnumber;
+		this.m1 = m1;
+		this.m2 = m2;
+		this.m3 = m3;
+}
+public float calculateAverage() {
+	return (m1+m2+m3)/3;
+}
+public void displayDetails() {
+	System.out.println("Roll Number:"+ rollnumber);
+	System.out.println("Name:"+ name);
+	System.out.println("Mark 1:"+ m1);
+	System.out.println("Mark 2:"+ m2);
+	System.out.println("Mark 3:"+ m3);
+	System.out.println("Average:"+ calculateAverage() );
+}
+}
+public class affan_1 {
+	public static void main(String[]args) {
+		Student Student1=new Student("Affan",8,40,41,42);
+		Student Student2=new Student("Affan X",80,04,14,24);
+		Student1.displayDetails();
+		Student2.displayDetails();
+		
+	}
+
+
+
 }
 
 
--- affan_2.java
+++ affan_2.java
@@ -1,12 +1,45 @@
-package affanP1;
-import java.util.Scanner;
-public class affan_2 {
-	public static void main(String [] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a number :");
-		int number=  sc.nextInt();
-		System.out.println("the number: "+number);
+
+
+	package affan;
+
+class Book{
+	String author;
+	String title;
+	 double price;
+	 int quantity;
+	 double total;
 	
+	Book( String title,String author , double price , int quantity ){
+		this.title = title;
+		this.author = author;
+		this.price= price;
+		this.quantity = quantity;
+}
+public double calculateTotal() {
+	return (price*quantity);
+}
+public void displayDetails() {
+	System.out.println("Title:"+ title);
+	System.out.println("Author:"+ author);
+	System.out.println("Price:"+ price);
+	System.out.println("Quantity:"+ quantity);
+	System.out.println("Total Value:"+ calculateTotal() );
+}
+}
+public class affan_2 {
+	public static void main(String[]args) {
+		Book book1=new Book("To Kill a Mockingbird", "Harper Lee", 14.99, 100);
+		Book book2=new Book("1984", "George Orwell", 12.99, 150);
+		book1.displayDetails();
+		book2.displayDetails();
+		
 	}
 
+
+
 }
+
+
+
+
+
