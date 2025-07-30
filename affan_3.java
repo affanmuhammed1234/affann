4cad24f9429a1d54030835c220d01fa9642f6ff5
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2025-07-30 14:58:32 +0530

    Add files via upload

--- affan_3.java
+++ affan_3.java
@@ -1,17 +1,30 @@
-package affanP1;
-import java.util.Scanner;
+package affan;
 
-public class affan_3 {
-	    public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.println("Enter a number: ");
-	        int n = scanner.nextInt();
-
-	        for (int i = n - 1; i >= 0; i--) {
-	            System.out.print(i + " ");
-	        }
-	    }
+class Shapes {
+	public int area(int side) {
+		System.out.println("I am Square");
+		return side*side;
 	}
-
-
-
+	public int area(int l, int b) {
+		System.out.println("I am rectangle");
+		return l*b;
+	}
+	public float area(float radius) {
+		System.out.println("I am circle");
+		return 3.14f*radius*radius;
+	}
+	public class affan_2 {
+		public static void main(String[]args) {
+			Shapes Shape = new Shapes();
+			int rectangleArea = Shape.area(5,6);
+			System.out.println("area of rectangle;"+rectangleArea);
+			int squareArea = Shape.area(5);
+			System.out.println("area of square;"+squareArea);
+			float circleArea = Shape.area(4.5f);
+			System.out.println("area of circle;"+circleArea);
+			
+			
+			
+	}
+}
+}
